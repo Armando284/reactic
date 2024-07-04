57323e81bfa8236fb00a28958cdde0b233ef5490
Armando <72764913+Armando284@users.noreply.github.com>
2024-07-04 14:07:50 -0400

    Feat: Adds algorithm to loop through units of work
    Uses a DSA Fiber Tree, each node has a conection to a parent, a child? and a sibling?

--- src/reactic/reactic.ts
+++ src/reactic/reactic.ts
@@ -1,4 +1,4 @@
-import { ReacticElement, Props, ReacticTextElement } from './typings'
+import { ReacticElement, ReacticTextElement, Fiber } from './typings'
 
 function createElement(
   type: keyof HTMLElementTagNameMap,
@@ -26,26 +26,31 @@ function createTextElement(text: string): ReacticTextElement {
   }
 }
 
-function render(element: ReacticElement, container: HTMLElement | Text) {
-  const node =
-    element.type === 'TEXT_ELEMENT'
+function createDom(fiber: Fiber): HTMLElement | Text {
+  const dom =
+    fiber.type == 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type)
+      : document.createElement(fiber.type)
 
   const isProperty = (key: string) => key !== 'children'
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      node[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
 
-  element.props.children.forEach((child) => {
-    // TODO: What happens if the container is a Text node
-    render(child, node)
-  })
+  return dom
+}
 
-  container.appendChild(node)
+function render(element: ReacticElement, container: HTMLElement) {
+  // TODO set next unit of work
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
 }
 
 let nextUnitOfWork = null
@@ -61,11 +66,51 @@ function workLoop(deadline: IdleDeadline) {
 
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
-  // add the element to the DOM
-  // create the fibers for the element’s children
-  // select the next unit of work
+function performUnitOfWork(fiber: Fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // create new fibers
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling: Fiber = null
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber: Fiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+    index++
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 export const Reactic = {
