0f04fdaa3f83413e4b64e5bc2b5d78346a198d29
Armando <72764913+Armando284@users.noreply.github.com>
2024-07-03 21:50:16 -0400

    Feat: Adds createElement
    Adds comment necesary for babel to use my createElement with jsx

--- reactic.js
+++ reactic.js
@@ -1,2 +1,31 @@
 'use strict'
-console.log('Hello, World!')
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object'
+          ? child
+          : createTextElement(child)),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    },
+  }
+}
+
+const Reactic = {
+  createElement,
+}
+
+console.log(Reactic.createElement('div', null, 'h1', 'p'));
+
