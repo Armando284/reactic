5673aa5114beaf8cbdc17cdfec1892f74878263b
Armando <72764913+Armando284@users.noreply.github.com>
2024-07-06 17:21:26 -0400

    Feat: Adds working example web page

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,41 @@ function Counter() {
   const [state, setState] = Reactic.useState(1)
   return (
     <main>
-      <h1>Count: {state}</h1>
-      <div className="button-group">
-        <button onClick={() => setState((c) => c - 1)}>-</button>
-        <button onClick={() => setState((c) => c + 1)}>+</button>
-      </div>
+      <section className="hero">
+        <h1>
+          Reactic
+          <img
+            src="../src/assets/reactic.png"
+            alt=""
+            width="72"
+          />
+        </h1>
+        <span>
+          A lightweight, <strong>TypeScript</strong> custom-built React-like
+          library designed to help developers understand the core concepts of{' '}
+          <strong>React</strong>.
+        </span>
+      </section>
+      <article className="counter">
+        <h3>Count: {state}</h3>
+        <div className="button-group">
+          <button onClick={() => setState((c) => c - 1)}>-</button>
+          <button onClick={() => setState((c) => c + 1)}>+</button>
+        </div>
+      </article>
+      <footer>
+        <ul>
+          <li>
+            Public repo:{' '}
+            <a
+              href="https://github.com/Armando284/reactic"
+              target="_blank"
+              rel="noopener noreferrer">
+              Armando284/reactic
+            </a>
+          </li>
+        </ul>
+      </footer>
     </main>
   )
 }
